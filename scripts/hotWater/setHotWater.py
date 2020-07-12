d7c4cb890e72a0f317a3639351836a02a016afbe
lukJZu <lukjzu@hotmail.co.uk>
2020-07-12 01:17:51 +0100

    added boost

--- scripts/hotWater/setHotWater.py
+++ scripts/hotWater/setHotWater.py
@@ -1,7 +1,7 @@
-import os
+import os, sys, time, json, csv
 import RPi.GPIO as GPIO
 from datetime import datetime
-import pandas as pd
+# import pandas as pd
 import iso8601
 from pathlib import Path
 
@@ -11,28 +11,95 @@ pinNo = 17
 
 GPIO.setup(pinNo, GPIO.OUT)
 
-#get the current time
-timeNow = datetime.now().astimezone()
-
-#read in the csv as pandas dataframe
-scheduleDF = pd.read_csv(os.path.join(Path.home(), 'data', 'hotWaterSchedule.csv'))
-scheduleDF['time'] = scheduleDF['time'].apply(iso8601.parse_date)
-
-#scheduleDF['time'] = pd.to_datetime(scheduleDF['time'])
-#scheduleDF.time = scheduleDF.time.dt.tz_localize(tzlocal())
-
-subDF = scheduleDF[(scheduleDF.time<timeNow)]
-row = subDF[subDF.time == subDF.time.max()]
-
-if not row.size:
-	state = 0
-elif row.iloc[0, 1]:
-	state = 1
-elif not row.iloc[0, 1]:
-	state = 0
+boostJSON = os.path.join(Path.home(), 'data', 'boostStates.json')
+scheduleCSV = os.path.join(Path.home(), 'data', 'hotWaterSchedule.csv')
+secondsInterval = 20
 
 
-if state:
-	GPIO.output(pinNo, GPIO.LOW)
-elif not state:
-	GPIO.output(pinNo, GPIO.HIGH)
+def condenseTimes(timeStates:list):
+    prevState = 0
+    condensedTimes = []
+    for timeState in timeStates:
+        if len(timeState) <= 1:
+            continue
+        if timeState[-1] == 'True':
+            state = True
+            startTime = iso8601.parse_date(timeState[0])
+            prevStates = timeState[1:]
+        elif timeState[-1] == 'False':
+            state = False
+            if prevState:
+                endTime = iso8601.parse_date(timeState[0])
+                prevStates = [False if a == 'False' else True for a in prevStates]
+                condensedTimes.append((startTime, endTime, prevStates
+                            ))
+        prevState = state
+
+    return condensedTimes
+
+def checkAgainstSchedule():
+	#open the schedule csv file
+	with open(scheduleCSV, 'r') as f:
+		schedule = list(csv.reader(f))[1:]
+	#sort the schedule into list of start-end times
+	condensedTimes = condenseTimes(schedule)
+	#assume off initially
+	state = False
+	#iterate over the timeframes to check whethere timenow is within any timeframe
+	for timeframe in condensedTimes:
+		if timeframe[0] < timeNow < timeframe[1]:
+			state = True
+			break
+
+	return state
+
+#getting the boost states from the json file
+def checkBoostState():
+	with open(boostJSON, 'r') as f:
+		boostStates = json.load(f)
+	return boostStates
+
+#update the boost json file by setting boost to off
+def turnOffBoost():
+	boostStates = checkBoostState()
+	boostStates['hotWater']['state'] = False
+	with open(boostJSON, 'w') as f:
+		json.dump(boostStates, f)
+
+try:
+	# prevState = False
+	while True:
+		#get the current time
+		timeNow = datetime.now().astimezone()
+
+		#get the boostState
+		boostStates = checkBoostState()['hotWater']
+		bState = boostStates['state']
+		#if boost is on
+		if bState:
+			#get the end time
+			endTime = datetime.fromisoformat(boostStates['endTime'])
+			#if end time hasn't passed, then set state as on
+			if timeNow < endTime:
+				state = True
+			#if end time has passed, turn boost off by updating json, 
+			#then set state according to schedule
+			elif endTime < timeNow:
+				turnOffBoost()
+				state = checkAgainstSchedule()
+		#if boost is off, set state according to schedule
+		else:
+			state = checkAgainstSchedule()
+
+		#check if previous state is the same as current state
+		#if not, then set the state
+		# if prevState != state:
+		GPIO.output(pinNo, not state)
+
+		#pause for x seconds
+		time.sleep(secondsInterval)
+		# prevState = state
+
+
+except KeyboardInterrupt:
+	sys.exit()
--- scripts/measureBoiler.py
+++ scripts/measureBoiler.py
@@ -5,6 +5,7 @@ import RPi.GPIO as GPIO
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(17, GPIO.OUT)
+GPIO.setup(18, GPIO.OUT)
 GPIO.setup(23, GPIO.IN)
 
 prevState = -1
@@ -14,19 +15,24 @@ csvFile = os.path.join(homeDir, 'data', 'boilerState.csv')
 
 try:
 	while True:
-		if not GPIO.input(17):
-			state = GPIO.input(23)
-		else:
-			state = GPIO.HIGH
+#		if not GPIO.input(17):
+#			state = GPIO.input(23)
+#		else:
+#			state = GPIO.HIGH
+		
+		#storing state of the hotWater, heating and boiler
+		hotWaterState = not GPIO.input(17)
+		heatingState  = bool(GPIO.input(18))
+		boilerState   = not GPIO.input(23)
 	
-		if state != prevState or datetime.datetime.now() - prevTime > datetime.timedelta(hours = 12):
+		if boilerState != prevState or datetime.datetime.now() - prevTime > datetime.timedelta(hours = 12):
 			with open(csvFile, 'a') as f:
-				timeNow = datetime.datetime.now().replace(microsecond = 0)
-				f.write(f'{timeNow.isoformat()},{not state}\n')
+				timeNow = datetime.datetime.now().replace(microsecond = 0).astimezone()
+				f.write(f'{timeNow.isoformat()},{hotWaterState},{heatingState},{boilerState}\n')
 			prevTime = datetime.datetime.now()
 
-		prevState = state
+		prevState = boilerState
 
-		time.sleep(15)
+		time.sleep(30)
 except KeyboardInterrupt:
 	sys.exit()
